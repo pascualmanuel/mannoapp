22547bb86475789865ee0e742fb89f7bd40e77a2
pascualmanuel <manuelnpascual@gmail.com>
2022-12-22 16:31:56 -0300

    4Cat - texts

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -10,16 +10,16 @@ function Categories() {
       <div className="background-categories">
         <div className="categories-container">
           <div className="categories-left-container">
-            <p style={{marginBottom: "15px"}}>¿Necesitás una mano?</p>
+            <p style={{marginBottom: "15px"}}>¿Cómo podemos ayudarte hoy?</p>
             <h2>
               <span style={{color: "#5d80ff"}}> Cuatro </span>
-              categorías de expert@s, <br></br>una sola app.
+              categorías, <br></br> todas las soluciones.
             </h2>
             <p style={{marginTop: "10px"}}>
-              Contactá expert@s en tareas para el hogar,&nbsp;
-              <br className="br-right-p-categories" />
-              profes, gurús de la tecnología o handys{" "}
-              <br className="br-right-p-categories" /> arreglatodo.
+              Encuentra a los Experos más
+              <br className="br-right-p-categories" /> recomendados para tu
+              hogar, <br className="br-right-p-categories" /> clases o
+              tecnología.
             </p>
           </div>
           <div className="categories-right-container">
@@ -44,8 +44,7 @@ function Categories() {
                     // fontSize: "15px",
                   }}
                 >
-                  Contactá al expert@ ideal para resolver cualquier imprevisto
-                  en tu hogar.
+                  Electricistas, Plomeros, Gasistas, Cerrajeros y mucho más.
                 </p>
               </div>
               <div className="box">
@@ -70,8 +69,7 @@ function Categories() {
                     // fontSize: "15px",
                   }}
                 >
-                  Elegí entre los mejores expert@s en celulares, tablets o
-                  computadoras.
+                  Repara tu Teléfono móvil, Tablet o Computadora.
                 </p>
               </div>
               <div className="box">
@@ -94,8 +92,7 @@ function Categories() {
                     // fontSize: "15px",
                   }}
                 >
-                  Potenciá tu canto, tu inglés, o tu entrenamiento físico! Está
-                  en tus manos.
+                  Aprende un idioma, música o comienza a entrenar.
                 </p>
               </div>
               <div className="box">
@@ -120,8 +117,8 @@ function Categories() {
                     // fontSize: "15px",
                   }}
                 >
-                  Esta categoría no la entiendo muy bien todavía. Pero acá va un
-                  breve texto.
+                  Elige tu Handyman para esas pequeñas tareas de rápida
+                  solución.
                 </p>
               </div>
             </div>
