6c83aa2c7410b41e492fb69ac01ff55439a69e27
pascualmanuel <manuelnpascual@gmail.com>
2024-06-17 13:11:47 -0300

    reviews ✅

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,4 +1,7 @@
 import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import FiveStars from "../Assets/five-stars.svg";
 import Mujer1 from "../Assets/Personitas/Mujer1.png";
 import Mujer2 from "../Assets/Personitas/Mujer2.png";
 import Mujer3 from "../Assets/Personitas/Mujer3.png";
@@ -11,9 +14,95 @@ import Hombre2 from "../Assets/Personitas/Hombre2.png";
 import Hombre3 from "../Assets/Personitas/Hombre3.png";
 import Hombre4 from "../Assets/Personitas/Hombre4.png";
 import Hombre5 from "../Assets/Personitas/Hombre5.png";
-import FiveStars from "../Assets/five-stars.svg";
-import AliceCarousel from "react-alice-carousel";
-import "react-alice-carousel/lib/alice-carousel.css";
+
+const reviewsData = [
+  {
+    img: Hombre3,
+    name: "Matías",
+    text: "Super recomendable, explico el paso a paso de lo que hacia y me dio consejos a tener en cuenta para el mantenimiento de la cocina.",
+  },
+  {
+    img: Mujer2,
+    name: "Paula",
+    text: "Impecable. Vino al toque. Instaló la hamaca. Quedó perfecto. gracias!",
+  },
+  {
+    img: Mujer7,
+    name: "Guadalupe",
+    text: "Super puntual! Prolijo, profesional, süper recomendable. quedó todo perfecto. Muchas gracias",
+  },
+  {
+    img: Hombre2,
+    name: "Fernando",
+    text: "Fue puntual, trabajo my bien, profesional.",
+  },
+  {
+    img: Mujer1,
+    name: "Monica",
+    text: "Excelente todo , hasta me trajo una aspiradora para tener mientras arreglaba la mia. IMPECABLE!!!",
+  },
+  { img: Hombre3, name: "Martín", text: "Muy responsable. Lo recomiendo!" },
+  {
+    img: Mujer6,
+    name: "Antonella",
+    text: "En un ratito me solucionó el problema de la persiana para que funcione correctamente. Süper amable y profesional.",
+  },
+  {
+    img: Hombre1,
+    name: "Adrián",
+    text: "Fue muy amable y soluciono el inconveniente muy rápido. Recomendable",
+  },
+  {
+    img: Mujer4,
+    name: "Adela",
+    text: "Impecable. Cometi un error con los materiales y Julián me ayudó a solucionarlo. El trabajo quedó impecable",
+  },
+  {
+    img: Mujer5,
+    name: "Roxana",
+    text: "Solucionaron todo con eficiencia y precio justo. Recomiendo!",
+  },
+  { img: Hombre4, name: "Juan", text: "Muy profesional y atento" },
+  { img: Mujer3, name: "Carmen", text: "Buen trabajo, ordenado, recomendable" },
+  { img: Hombre5, name: "Manuel", text: "Totalmente recomendable." },
+  {
+    img: Mujer6,
+    name: "Karina",
+    text: "Excelente trabajo, atención y asesoramiento,",
+  },
+  {
+    img: Mujer7,
+    name: "María Mercedes",
+    text: "Desinstaló un aire acondicionado viejo e instalo uno nuevo. Todo perfecto",
+  },
+  {
+    img: Mujer1,
+    name: "Graciela",
+    text: "Excelente profesional. Puntual y prolijo",
+  },
+];
+
+const ReviewItem = ({ img, name, text }) => (
+  <div className="item">
+    <div className="reviews-box">
+      <img
+        src={img}
+        alt="Avatar"
+        width={55}
+        height={55}
+        style={{ marginLeft: "15px", marginTop: "15px" }}
+      />
+      <h2 className="reviews-title">{name}</h2>
+      <p className="reviews-text">{text}</p>
+      <img
+        src={FiveStars}
+        alt="FiveStars"
+        height={20}
+        style={{ marginLeft: "20px" }}
+      />
+    </div>
+  </div>
+);
 
 function Reviews() {
   const responsive = {
@@ -23,447 +112,43 @@ function Reviews() {
     1060: { items: 4 },
   };
 
-  const reviewsData = [
-    {
-      img: Hombre3,
-      name: "Matías",
-      text: "Super recomendable, explico el paso a paso de lo que hacia y me dio consejos a tener en cuenta para el mantenimiento de la cocina.",
-    },
-    {
-      img: Mujer2,
-      name: "Paula",
-      text: "Impecable. Vino al toque. Instaló la hamaca. Quedó perfecto. gracias!",
-    },
-    {
-      img: Mujer7,
-      name: "Guadalupe",
-      text: "Super puntual! Prolijo, profesional, süper recomendable. quedó todo perfecto. Muchas gracias",
-    },
-    {
-      img: Hombre2,
-      name: "Fernando",
-      text: "Fue puntual, trabajo my bien, profesional.",
-    },
-    {
-      img: Mujer1,
-      name: "Monica",
-      text: "Excelente todo , hasta me trajo una aspiradora para tener mientras arreglaba la mia. IMPECABLE!!!",
-    },
-    { img: Hombre3, name: "Martín", text: "Muy responsable. Lo recomiendo!" },
-    {
-      img: Mujer6,
-      name: "Antonella",
-      text: "En un ratito me solucionó el problema de la persiana para que funcione correctamente. Süper amable y profesional.",
-    },
-    {
-      img: Hombre1,
-      name: "Adrián",
-      text: "Fue muy amable y soluciono el inconveniente muy rápido. Recomendable",
-    },
-    {
-      img: Mujer4,
-      name: "Adela",
-      text: "Impecable. Cometi un error con los materiales y Julián me ayudó a solucionarlo. El trabajo quedó impecable",
-    },
-    {
-      img: Mujer5,
-      name: "Roxana",
-      text: "Solucionaron todo con eficiencia y precio justo. Recomiendo!",
-    },
-    { img: Hombre4, name: "Juan", text: "Muy profesional y atento" },
-    {
-      img: Mujer3,
-      name: "Carmen",
-      text: "Buen trabajo, ordenado, recomendable",
-    },
-    { img: Hombre5, name: "Manuel", text: "Totalmente recomendable." },
-    {
-      img: Mujer6,
-      name: "Karina",
-      text: "Excelente trabajo, atención y asesoramiento,",
-    },
-    {
-      img: Mujer7,
-      name: "María Mercedes",
-      text: "Desinstaló un aire acondicionado viejo e instalo uno nuevo. Todo perfecto",
-    },
-    {
-      img: Mujer1,
-      name: "Graciela",
-      text: "Excelente profesional. Puntual y prolijo",
-    },
-  ];
-
-  const items = [
-    <div className="item" data-value="1">
-      <div className="reviews-box">
-        <img
-          src={Hombre3}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Matías</h2>
-        <p className="reviews-text">
-          Super recomendable, explico el paso a paso de lo que hacia y me dio
-          consejos a tener en cuenta para el mantenimiento de la cocina.
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="2">
-      <div className="reviews-box">
-        <img
-          src={Mujer2}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Paula</h2>
-        <p className="reviews-text">
-          Impecable. Vino al toque. Instaló la hamaca. Quedó perfecto. gracias!
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="3">
-      <div className="reviews-box">
-        <img
-          src={Mujer7}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Guadalupe</h2>
-        <p className="reviews-text">
-          Super puntual! Prolijo, profesional, süper recomendable. quedó todo
-          perfecto. Muchas gracias
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="4">
-      <div className="reviews-box">
-        <img
-          src={Hombre2}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Fernando</h2>
-        <p className="reviews-text">
-          Fue puntual, trabajo my bien, profesional.
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="5">
-      <div className="reviews-box">
-        <img
-          src={Mujer1}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Monica</h2>
-        <p className="reviews-text">
-          Excelente todo , hasta me trajo una aspiradora para tener mientras
-          arreglaba la mia. IMPECABLE!!!
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="6">
-      <div className="reviews-box">
-        <img
-          src={Hombre3}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Martín</h2>
-        <p className="reviews-text">Muy responsable. Lo recomiendo!</p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-
-    <div className="item" data-value="7">
-      <div className="reviews-box">
-        <img
-          src={Mujer6}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Antonella</h2>
-        <p className="reviews-text">
-          En un ratito me solucionó el problema de la persiana para que funcione
-          correctamente. Süper amable y profesional.
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="8">
-      <div className="reviews-box">
-        <img
-          src={Hombre1}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Adrián</h2>
-        <p className="reviews-text">
-          Fue muy amable y soluciono el inconveniente muy rápido. Recomendable
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-
-    <div className="item" data-value="9">
-      <div className="reviews-box">
-        <img
-          src={Mujer4}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Adela</h2>
-        <p className="reviews-text">
-          Impecable. Cometi un error con los materiales y Julián me ayudó a
-          solucionarlo. El trabajo quedó impecable
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="10">
-      <div className="reviews-box">
-        <img
-          src={Mujer5}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Roxana</h2>
-        <p className="reviews-text">
-          Solucionaron todo con eficiencia y precio justo. Recomiendo!
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-
-    <div className="item" data-value="11">
-      <div className="reviews-box">
-        <img
-          src={Hombre4}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Juan</h2>
-        <p className="reviews-text">Muy profesional y atento</p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="12">
-      <div className="reviews-box">
-        <img
-          src={Mujer3}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Carmen</h2>
-        <p className="reviews-text">Buen trabajo, ordenado, recomendable</p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="13">
-      <div className="reviews-box">
-        <img
-          src={Hombre5}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Manuel</h2>
-        <p className="reviews-text">Totalmente recomendable.</p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="14">
-      <div className="reviews-box">
-        <img
-          src={Mujer6}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Karina</h2>
-        <p className="reviews-text">
-          Excelente trabajo, atención y asesoramiento,
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="15">
-      <div className="reviews-box">
-        <img
-          src={Mujer7}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">María Mercedes</h2>
-        <p className="reviews-text">
-          Desinstaló un aire acondicionado viejo e instalo uno nuevo. Todo
-          perfecto
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-    <div className="item" data-value="16">
-      <div className="reviews-box">
-        <img
-          src={Mujer1}
-          alt="Avatar"
-          width={55}
-          height={55}
-          style={{ marginLeft: "15px", marginTop: "15px" }}
-        />
-        <h2 className="reviews-title">Graciela</h2>
-        <p className="reviews-text">
-          Excelente profesional. <br></br>Puntual y prolijo
-        </p>
-        <img
-          src={FiveStars}
-          alt="FiveStars"
-          height={20}
-          style={{ marginLeft: "20px" }}
-        />
-      </div>
-    </div>,
-  ];
+  const items = reviewsData.map((review, index) => (
+    <ReviewItem
+      key={index}
+      img={review.img}
+      name={review.name}
+      text={review.text}
+    />
+  ));
 
   return (
-    <>
-      <div
-        style={{
-          backgroundColor: "rgba(247, 248, 248, 1)",
-          paddingBottom: "100px",
-        }}
-      >
-        <div className="reviews-container">
-          <p>Lo que dicen los Usuarios</p>
-          <h2>
-            Comunidad
-            <span style={{ color: "#5d80ff" }}> Manno. </span>
-          </h2>
-        </div>
-        <AliceCarousel
-          mouseTracking
-          items={items}
-          responsive={responsive}
-          controlsStrategy="alternate"
-          infinite={true}
-          autoPlay={true}
-          autoPlayInterval={2000}
-          disableButtonsControls={true}
-          disableDotsControls={true}
-          autoPlayStrategy={"none"}
-          paddingLeft={50}
-        />
+    <div
+      style={{
+        backgroundColor: "rgba(247, 248, 248, 1)",
+        paddingBottom: "100px",
+      }}
+    >
+      <div className="reviews-container">
+        <p>Lo que dicen los Usuarios</p>
+        <h2>
+          Comunidad
+          <span style={{ color: "#5d80ff" }}> Manno. </span>
+        </h2>
       </div>
-    </>
+      <AliceCarousel
+        mouseTracking
+        items={items}
+        responsive={responsive}
+        controlsStrategy="alternate"
+        infinite={true}
+        autoPlay={true}
+        autoPlayInterval={2000}
+        disableButtonsControls={true}
+        disableDotsControls={true}
+        autoPlayStrategy="none"
+        paddingLeft={50}
+      />
+    </div>
   );
 }
 
